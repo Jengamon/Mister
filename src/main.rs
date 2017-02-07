54797e492ffb1098f4108df0acdc720d287b031d
Bob Hostern <bob.hostern@gmail.com>
2017-02-07 12:05:48 -0600

    Start some type of renderer

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate slog;
 // extern crate specs;
+#[macro_use]
 extern crate gfx;
 extern crate gfx_device_gl as gfx_gl;
 extern crate gfx_window_glutin as gfx_window;
@@ -10,10 +11,24 @@ extern crate mister_core;
 
 use gfx::format::{DepthStencil, Rgba8};
 
+// Our rudimentary rendering system. Hurrah for laziness.
+gfx_defines! {
+    vertex Vertex {
+        pos: [f32; 2] = "position",
+        tex: [f32; 2] = "tex_coords",
+    }
+
+    pipeline rgb {
+        vbuf: gfx::VertexBuffer<Vertex> = (),
+        picture: gfx::TextureSampler<Rgba8> = "Texture",
+        out: gfx::RenderTarget<Rgba8> = "Color",
+    }
+}
+
 fn main() {
     let builder = glutin::WindowBuilder::new()
         .with_title("MISTER".to_string());
-    let (window, device, factory, rtv, stv) = gfx_window::init::<Rgba8, DepthStencil>(builder);
+    let (window, mut device, mut factory, rtv, stv) = gfx_window::init::<Rgba8, DepthStencil>(builder);
     println!("Main function!");
 
     // TODO: Make a rudimentary rendering system to quickly draw an image to the screen.
@@ -27,8 +42,13 @@ fn main() {
             }
         }
 
+        let enc: gfx::Encoder<gfx_gl::Resources, _> = factory.create_command_buffer().into();
+
         // draw everything here
 
+        enc.flush();
+        
+        device.cleanup();
         window.swap_buffers().unwrap();
     }
 }
---
+++ src/test.frag
@@ -0,0 +1,12 @@
+#version 140
+
+in vec4 v_color;
+in vec4 v_tex_coords;
+
+uniform sampler2D Texture;
+
+out vec4 Color;
+
+void main() {
+  Color = v_color * texture2D(Texture, v_tex_coords);
+}
