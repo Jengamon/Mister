568d42e6316e6cf32a2072670d37d5d02bbbed9e
Bob Hostern <bob.hostern@gmail.com>
2017-02-10 06:13:25 -0600

    A sketch of a GUI model

---
+++ mister_gui/src/layout.rs
@@ -0,0 +1,2 @@
+//! This module provides a higher level concepts wrapper around Cassowary.
+use cassowary;
---
+++ mister_gui/src/render.rs
@@ -0,0 +1,12 @@
+//! Renders the GUI to screen
+use super::Object;
+
+/// Renders a Renderable to the screen
+trait Renderer {
+
+}
+
+/// Something that can be rendered
+trait Renderable {
+
+}
